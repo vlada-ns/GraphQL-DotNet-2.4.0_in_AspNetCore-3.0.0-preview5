7828ee1a4fc413f7e2a555e7c54f1a37b7a9ae97
Vladimir Asic <v.asic@levi9.com>
2019-06-04 14:34:00 +0200

    Removed from project all classes that have any relationship with GraphQL.EntityFramework excapt GraphQlController class in controllers

--- GraphQL_1/GraphQL/ContextFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿using GraphQL_1.Data;
-
-namespace GraphQL_1.GraphQL
-{
-    public class ContextFactory
-    {
-        public AppDbContext BuildContext() => DbContextBuilder.BuildDbContext();
-    }
-}
